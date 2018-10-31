2a74bb47ac59a12c28bd6db4cfab71273399a34d
fkp <2866137+fkp@users.noreply.github.com>
2018-10-31 22:37:32 +0000

    getting started

---
+++ canvas.js
@@ -0,0 +1,95 @@
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+/*c.fillStyle = 'rgba(255,0,0,0.5)';
+c.fillRect(100,100,100,100);
+c.fillStyle = 'rgba(0,0,255,0.5)';
+c.fillRect(400,100,100,100);
+*/
+
+// Lines
+/*c.beginPath();
+c.moveTo(50,300);
+c.lineTo(300,100);
+c.lineTo(400,300);
+c.strokeStyle = "#fa34a3";
+c.stroke();*/
+
+// Arcs
+/*for (var i =0; i < 300; i++)
+{
+	var x = Math.random() * window.innerWidth;
+	var y = Math.random() * window.innerHeight;
+	
+	c.beginPath();
+	c.arc(x,y,30,0, Math.PI * 2, false);
+	c.strokeStyle = 'blue'
+	c.stroke();
+}*/
+
+
+function Circle(x, y, dx, dy, radius)
+{
+	this.x = x;
+	this.y = y;
+	this.dx = dx;
+	this.dy = dy;
+	this.radius = radius;
+	
+	this.draw = function()
+	{
+		c.beginPath();
+		c.arc(this.x,this.y,this.radius,0, Math.PI * 2, false);
+		c.strokeStyle = 'blue';
+		c.stroke();
+	}
+	
+	this.update = function()
+	{
+		if (this.x+this.radius>innerWidth || this.x-radius<0)
+		{
+			this.dx = -this.dx;
+		}
+	
+		if (this.y+this.radius>innerHeight || this.y-this.radius<0)
+		{
+			this.dy = -this.dy;
+		}
+	
+		this.x+=this.dx;
+		this.y+=this.dy;
+		
+		this.draw();
+	}
+}
+
+var circleArray = [];
+var radius = 30;
+for (var i =0; i < 30; i++)
+{
+	circleArray.push(new Circle(Math.random() * (innerWidth-radius*2)+radius,
+		Math.random() * (innerHeight-radius*2)+radius,
+		(Math.random() - 0.5) * 8,
+		(Math.random() - 0.5) * 8,
+		radius));
+}
+
+
+function animate()
+{
+	requestAnimationFrame(animate);
+
+	c.clearRect(0,0,innerWidth, innerHeight);
+	
+	for (var i=0; i<circleArray.length; i++)
+	{
+		circleArray[i].update();
+	}
+	
+}
+
+animate();
