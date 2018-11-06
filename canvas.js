027b28aa1bf55e9f76d050d58162113b86b46e7b
fkp <2866137+fkp@users.noreply.github.com>
2018-11-06 21:24:42 +0000

    fixup

--- canvas.js
+++ canvas.js
@@ -5,12 +5,6 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
-/*c.fillStyle = 'rgba(255,0,0,0.5)';
-c.fillRect(100,100,100,100);
-c.fillStyle = 'rgba(0,0,255,0.5)';
-c.fillRect(400,100,100,100);
-*/
-
 // Lines
 /*c.beginPath();
 c.moveTo(50,300);
@@ -19,32 +13,52 @@ c.lineTo(400,300);
 c.strokeStyle = "#fa34a3";
 c.stroke();*/
 
-// Arcs
-/*for (var i =0; i < 300; i++)
+var mouse = {
+	x: undefined,
+	y: undefined
+}
+
+window.addEventListener('mousemove',
+	function(event) {
+	mouse.x = event.x;
+	mouse.y = event.y;
+})
+
+window.addEventListener('resize', function()
 {
-	var x = Math.random() * window.innerWidth;
-	var y = Math.random() * window.innerHeight;
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
 	
-	c.beginPath();
-	c.arc(x,y,30,0, Math.PI * 2, false);
-	c.strokeStyle = 'blue'
-	c.stroke();
-}*/
+	init();
+});
 
+var hitTest = 50;
+var maxRadius = 40;
 
-function Circle(x, y, dx, dy, radius)
+var colourArray = [
+	'#ffaa33',
+	'#99ffaa',
+	'#00ff00',
+	'#4411aa',
+	'#ff1100'];
+
+function Circle(x, y, dx, dy, radius, minRadius)
 {
 	this.x = x;
 	this.y = y;
 	this.dx = dx;
 	this.dy = dy;
 	this.radius = radius;
+	this.colour = colourArray[Math.floor(Math.random() * colourArray.length)];
+	this.minRadius = radius;
 	
 	this.draw = function()
 	{
 		c.beginPath();
 		c.arc(this.x,this.y,this.radius,0, Math.PI * 2, false);
-		c.strokeStyle = 'blue';
+		//c.strokeStyle = 'blue';
+		c.fillStyle = this.colour;
+		c.fill();
 		c.stroke();
 	}
 	
@@ -63,19 +77,87 @@ function Circle(x, y, dx, dy, radius)
 		this.x+=this.dx;
 		this.y+=this.dy;
 		
+		if (Math.abs(mouse.x - this.x) < hitTest && Math.abs(mouse.y - this.y) < hitTest && this.radius < maxRadius) {
+			this.radius +=1;
+		} else if (this.radius > this.minRadius) {
+			this.radius -=1;
+		}
+		
 		this.draw();
 	}
 }
 
-var circleArray = [];
-var radius = 30;
-for (var i =0; i < 30; i++)
+function Rectangle(x, y, dx, dy, height, width)
 {
-	circleArray.push(new Circle(Math.random() * (innerWidth-radius*2)+radius,
-		Math.random() * (innerHeight-radius*2)+radius,
-		(Math.random() - 0.5) * 8,
-		(Math.random() - 0.5) * 8,
-		radius));
+	this.x = x;
+	this.y = y;
+	this.dx = dx;
+	this.dy = dy;
+	this.height = height;
+	this.width = width;
+	
+	this.draw = function()
+	{
+		c.fillStyle = 'rgba(255,0,0,0.5)';
+		c.fillRect(this.x,this.y,width,height);
+	}
+	
+	this.update = function()
+	{
+		if (this.x+this.width>innerWidth || this.x-width<0)
+		{
+			this.dx = -this.dx;
+		}
+	
+		if (this.y+this.height>innerHeight || this.y-this.height<0)
+		{
+			this.dy = -this.dy;
+		}
+	
+		this.x+=this.dx;
+		this.y+=this.dy;
+		
+		this.draw();
+	}
+}
+
+
+var objectsArray = [];
+
+var speedMultiplier = 2;
+
+var rectHeight = 30;
+var rectWidth = 30;
+
+var noCircles = 100;
+var noRectangles = 10;
+
+init();
+
+function init()
+{
+	objectsArray = [];
+	
+	for (var i =0; i < noCircles; i++)
+	{
+		var radius = Math.random() * 6 + 1;
+		
+		objectsArray.push(new Circle(Math.random() * (innerWidth-radius*2)+radius,
+			Math.random() * (innerHeight-radius*2)+radius,
+			(Math.random() - 0.5) * speedMultiplier,
+			(Math.random() - 0.5) * speedMultiplier,
+			radius));
+	}
+
+	for (var i =0; i < noRectangles; i++)
+	{
+		objectsArray.push(new Rectangle(Math.random() * (innerWidth-rectWidth*2)+rectWidth,
+			Math.random() * (innerHeight-rectHeight*2)+rectHeight,
+			(Math.random() - 0.5) * speedMultiplier,
+			(Math.random() - 0.5) * speedMultiplier,
+			rectWidth,
+			rectHeight));
+	}
 }
 
 
@@ -85,9 +167,9 @@ function animate()
 
 	c.clearRect(0,0,innerWidth, innerHeight);
 	
-	for (var i=0; i<circleArray.length; i++)
+	for (var i=0; i<objectsArray.length; i++)
 	{
-		circleArray[i].update();
+		objectsArray[i].update();
 	}
 	
 }
